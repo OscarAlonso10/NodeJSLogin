bb2cc092c6cb0b31cb26f3d534a4081e49c214ea
Oscar Alonso <oskralonso10@gmail.com>
2020-02-03 17:48:19 +0100

    register

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-var express = require('express');
-var app = express();
 var bodyParser = require('body-parser')
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+let express = require('express');
+var app = express()
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
 
 var users = {
-	"paco": "noni",
-	"oscar": "noni",
-};
+  'paco':'noni',
+  'oscar':'noni',
+}
 
-app.get('/login', function (req, res) {
+app.get('/login', function (req, res){
   res.render('loginform');
 });
 
-
-app.post('/login', function (req, res) {
-	var user ="";
-	var inputUser = req.body.username;
-	if(inputUser in users) {
-		if (req.body.password == users[inputUser]) {
-			user = inputUser;
-		}
-	}
- 	res.render('loginformPost',{'user':user});
-
+app.post('/login', function (req, res){
+  var inputUser = req.body.username;
+  var inputPass = req.body.password;
+  var user = "";
+  if (localStorage.getItem(inputUser) == inputPass) {
+        user = inputUser;
+  }
+  res.render('loginformPost', {user});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+app.get('/register', function (req, res){
+  res.render('registerform');
+});
+
+app.post('/register', function (req, res){
+  var inputUser = req.body.username;
+  var inputPass = req.body.password;
+  localStorage.setItem(inputUser, inputPass);
+  res.render('registerformPost');
+});
+
+app.listen(3000, function(){
+  console.log('Exameple app listening on port 3000');
 });
